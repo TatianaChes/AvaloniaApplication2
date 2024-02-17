cce36381d76d26946ff66c3cebb3b6856d9235a2
tatia <tatia@LAPTOP-TQGJCAMJ>
2024-02-17 16:07:39 +0500

    тестовый коммит 2

--- AvaloniaApplication2/Views/ThirdPageView.axaml.cs
+++ AvaloniaApplication2/Views/ThirdPageView.axaml.cs
@@ -187,7 +187,7 @@ namespace AvaloniaApplication2.Views
             }
             else
             {
-                StaticClass.ShowMessageBox("Установите количество и выберите файлы!", "Предупреждение", ButtonEnum.Ok); // что мне делать
+                StaticClass.ShowMessageBox("Установите количество и выберите файлы!", "Предупреждение", ButtonEnum.Ok); // помогите
             }
         }
     }
